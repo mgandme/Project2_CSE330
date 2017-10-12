95f8768177015b4ffdf7df713c37011624b86bca
Ryan <rpatter5@asu.edu>
2017-10-12 14:15:49 -0700

    sem.h Has P(semaphore) method.

--- sem.h
+++ sem.h
@@ -1,11 +1,20 @@
 #include "threads.h"
 #include <stdlib.h>
 
-struct semaphore {
+typedef struct semaphore {
 	int val;
 	Queue *q;
-};
+}semaphore;
 
 void InitSem(semaphore sem, int x) {
-	val = x;
+	sem.val = x;
+}
+
+int P(semaphore sem) {
+	sem.val--;
+	if(sem.val > 0) {
+		return -1;
+	}
+	return 1;
+
 }
